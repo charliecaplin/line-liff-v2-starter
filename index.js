3bae63ad055557413f544ab26357add187da0c38
charliecaplin <91089814+charliecaplin@users.noreply.github.com>
2021-09-24 02:08:25 +0700

    update
    update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const https = require('ssl-express-www'); //buat selalu redirect ke https.
 const app = express();
 const port = process.env.PORT || 5000;
 const myLiffId = process.env.MY_LIFF_ID;
 
-app.use(express.static('public'));
-
+app.use(https); //https.
+app.use(express.static('public')); // atau assets note: <folder name>
+app.use('/assets', express.static("public")); //note: ini buat access kalo misal nyimpen foto di assets <https://<appname>.herokuapp.com/assets/<path>
 app.get('/send-id', function(req, res) {
     res.json({id: myLiffId});
 });
